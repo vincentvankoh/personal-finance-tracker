ae2214d27b2fa2ac426e14ecaae04960c0e8b2a3
Danny <chungdanny63@yahoo.com>
2020-12-15 15:23:13 -0800

    created and checked routes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,21 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 
+const userRouter = require('./Routes/userRouter')
+const dataRouter = require('./Routes/dataRouter')
 
 const app = express()
 
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended : true}))
+
+
+app.use('/user', userRouter)
+app.use('/data', dataRouter)
+
+
+
 app.use(cors)
 app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
 app.get('/', (req, res) =>

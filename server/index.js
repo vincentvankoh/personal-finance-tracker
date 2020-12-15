5ca9531d85ef839468664f76df14fad9d86f29e3
Joseph <etoh101@gmail.com>
2020-12-15 15:24:08 -0800

    (add) installed library for passport and google oauth20

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) =>
 	res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
 );
 
+
+
 app.listen(3000, (err) => {
 	if (err) return console.log(err);
 	console.log('Server running and listening on 3000');

eafa967c99b218f757e8dd4d4a2ae6799b9b2bfa
Joseph <etoh101@gmail.com>
2020-12-15 15:27:14 -0800

    (merge) merged

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
-
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors);
 
 app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+
 app.get('/', (req, res) =>
 	res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
 );

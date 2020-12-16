0f0e7ab7e3d1ce905ae5d8c5e408a8aa36b7290d
vincentvankoh <yunskoh@gmail.com>
2020-12-16 00:47:12 -0500

    hotfix for logout routes

--- client/plan.js
+++ client/plan.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Sidebarfooter from "./sidebarfooter";
 import Topnav from "./topnav";
@@ -7,30 +6,7 @@ import Topnav from "./topnav";
 export default function Plan() {
     return(
         <div className="sb-nav-fixed">
-            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-                <a className="navbar-brand" href="index.html">Financial Planner</a>
-                <button className="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i className="fas fa-bars"></i></button>
-                <form className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                    <div className="input-group">
-                        <input className="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
-                        <div className="input-group-append">
-                            <button className="btn btn-primary" type="button"><i className="fas fa-search"></i></button>
-                        </div>
-                    </div>
-                </form>
-    
-                <ul className="navbar-nav ml-auto ml-md-0">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fas fa-user fa-fw"></i></a>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                            <a className="dropdown-item" href="#">Settings</a>
-                            <a className="dropdown-item" href="#">Activity Log</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="login.html">Logout</a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
+            <Topnav />
             <div id="layoutSidenav">
                 <Sidebar />
                 <Sidebarfooter />

d2caf2e99e8daf929f05165765719798e5a6cfed
vincentvankoh <yunskoh@gmail.com>
2020-12-16 00:28:24 -0500

    hotfix on classnames

--- client/tables.js
+++ client/tables.js
@@ -6,34 +6,34 @@ import Topnav from "./topnav";
 
 export default function Tables() {
   return(
-    <div class="sb-nav-fixed">
+    <div className="sb-nav-fixed">
         <Topnav />
         <div id="layoutSidenav">
             <Sidebar />
             <Sidebarfooter />
             <div id="layoutSidenav_content">
                 <main>
-                    <div class="container-fluid">
-                        <h1 class="mt-4">Tables</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Tables</li>
+                    <div className="container-fluid">
+                        <h1 className="mt-4">Tables</h1>
+                        <ol className="breadcrumb mb-4">
+                            <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li className="breadcrumb-item active">Tables</li>
                         </ol>
-                        <div class="card mb-4">
-                            <div class="card-body">
+                        <div className="card mb-4">
+                            <div className="card-body">
                                 DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
                                 <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
                                 .
                             </div>
                         </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table mr-1"></i>
+                        <div className="card mb-4">
+                            <div className="card-header">
+                                <i className="fas fa-table mr-1"></i>
                                 DataTable Example
                             </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <div className="card-body">
+                                <div className="table-responsive">
+                                    <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                         <thead>
                                             <tr>
                                                 <th>Name</th>

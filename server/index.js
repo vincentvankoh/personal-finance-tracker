eb34884b522aecc72153b3b0af61c244734fd3e2
Joseph <etoh101@gmail.com>
2020-12-15 21:10:04 -0800

    (update) modified oauth login, unique userid verification

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config()
+const db = require('./Models/FinancialModel')
 
 const userRouter = require('./Routes/userRouter')
 const dataRouter = require('./Routes/dataRouter')
@@ -26,8 +27,41 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/redirect"
   },
   (accessToken, refreshToken, profile, done) =>{
-	  console.log(profile)
-	  console.log('passport function')
+	  // console.log(profile)
+		// console.log('passport function')
+
+		//save user profile to users table
+		let userName = profile.displayName;
+		let pass = profile.id;
+		console.log(userName, pass)
+		//check if pass(unique id exist), if it does do nothing.
+		//if unique id doesn't insert one in db
+		let sqlQuery = "select * from users where password=\'"+pass+"\'";
+		db.query(sqlQuery, (err, response) => {
+			if(err) {
+				console.log(err.stack)
+				
+			} else {
+				if(response.rows.length === 0) {
+					//user doesn't exist, insert in db.
+					let createUser = `insert into Users (Username,Password) values ('${userName}', '${pass}')`;
+					db.query(createUser, (err, response) => {
+						if(err) {
+							console.log(err.stack)
+							
+						} else {
+							//if no err running query, new user should be created
+							console.log('USER MADE')
+						
+						}
+					})
+				} else {
+					//if unique pass is found, redirect
+					console.log('UNIQUE USER IS FOUND')
+					
+				}
+			}
+		})
   }
 //   function(accessToken, refreshToken, profile, cb) {
 //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -43,6 +77,19 @@ app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
 app.get('/', (req, res) =>
 	res.status(200).sendFile(path.resolve(__dirname, '../build/index.html'))
 );
+
+//global internal error
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(3000, (err) => {
 	if (err) return console.log(err);
 	console.log('Server running and listening on 3000');
